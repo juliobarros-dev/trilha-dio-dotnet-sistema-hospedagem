4d64785999ee3981ab4ab6add61e3ee2dcc11547
Julio Barros <juliobarros.dev@gmail.com>
2022-10-02 03:08:04 -0300

    Adiciona menu e input da dados

--- SistemaHospedagem/SistemaHospedagem/Program.cs
+++ SistemaHospedagem/SistemaHospedagem/Program.cs
@@ -3,23 +3,77 @@ using System.Text;
 
 Console.OutputEncoding = Encoding.UTF8;
 
-// Cria os modelos de hóspedes e cadastra na lista de hóspedes
-List<Pessoa> hospedes = new List<Pessoa>();
+Suite basicSuite = new("Basic", 1, 20.00m);
+Suite standardSuite = new("Standard", 3, 20.00m);
+Suite premiumSuite = new("Basic", 5, 20.00m);
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+List<Suite> hotelRooms = new List<Suite>() { basicSuite, standardSuite, premiumSuite };
 
-hospedes.Add(p1);
-hospedes.Add(p2);
+bool showMenu = true;
 
-// Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+while (showMenu)
+{
+  Console.WriteLine("Sistema de hospedagem\n");
+  Console.WriteLine("Escolha um opção pra prosseguir\n");
+  Console.WriteLine("1. Cadastrar hóspedes;");
+  Console.WriteLine("2. Encerrar programa.");
 
-// Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 10);
-reserva.CadastrarSuite(suite);
-reserva.CadastrarHospedes(hospedes);
+  switch (Console.ReadLine())
+  {
+    case "1":
+      Console.Clear();
+      Console.WriteLine("Sistema de hospedagem\n");
+      Console.WriteLine("Escolha sua suíte");
+      foreach (var suite in hotelRooms)
+      {
+        Console.WriteLine(suite.TipoSuite);
+      }
+      Console.WriteLine();
 
-// Exibe a quantidade de hóspedes e o valor da diária
-Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+      string inputSelectedSuite = Console.ReadLine().ToLower();
+
+      Suite selectedSuite = hotelRooms.Find(x => x.TipoSuite.ToLower() == inputSelectedSuite);
+
+      Console.Clear();
+      Console.WriteLine("Sistema de hospedagem\n");
+      Console.WriteLine($"A capacidade da suíte é de {selectedSuite.Capacidade} hóspedes.\n");
+
+      List<Pessoa> hospedes = new();
+
+      for (int index = 1; index <= selectedSuite.Capacidade; index++)
+      {
+        Console.Write("Digite o nome completo do hóspede: ");
+        string[] inputFullName = Console.ReadLine().Split(' ');
+        string firstName = inputFullName[0];
+        string lastName = inputFullName[1];
+
+        Pessoa hospede = new(firstName, lastName);
+        hospedes.Add(hospede);
+      }
+
+      Console.Write("Digite quantos dias deseja se hospedar: ");
+      int period = int.Parse(Console.ReadLine());
+
+      Reserva reservation = new(period);
+      reservation.CadastrarSuite(selectedSuite);
+      reservation.CadastrarHospedes(hospedes);
+
+      Console.WriteLine("\nHóspedes cadastrados");
+
+      foreach(Pessoa hospede in hospedes)
+      {
+        Console.WriteLine($"- {hospede.NomeCompleto};");
+      }
+
+      break;
+
+    case "2":
+      showMenu = false;
+      Console.WriteLine("Programa encerrado");
+      break;
+  }
+
+  Console.WriteLine("\nPressione qualquer tecla para continuar.");
+  Console.ReadLine();
+  Console.Clear();
+}
